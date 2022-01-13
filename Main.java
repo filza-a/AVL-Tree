34bbc0424d0a3f6cdc5193020b8ad1e50a1b1fd7
Filza Ahmad <64220634+filza-a@users.noreply.github.com>
2022-01-14 00:31:54 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package AVLtree;
+
+public class Main {
+    public static void main(String[] args) {
+        var tree = new AVLTree();
+        tree.insert(30);
+        tree.insert(50);
+        tree.insert(40);
+        tree.insert(9);
+        tree.insert(3);
+        tree.insert(12);
+    }
+
+}
